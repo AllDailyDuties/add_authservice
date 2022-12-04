91db3450753f01517130d11b729c9a8abe827983
Koen-sanders <koensanders@outlook.com>
2022-12-04 17:10:24 +0100

    rabbitmq connection test

--- AllDailyDuties-AuthService/Middleware/Messaging/RabbitMQConnection.cs
+++ AllDailyDuties-AuthService/Middleware/Messaging/RabbitMQConnection.cs
@@ -10,13 +10,60 @@ namespace AllDailyDuties_AuthService.Middleware.Messaging
         {
             IConnectionFactory connectionFactory = new ConnectionFactory
             {
-                HostName = "127.0.0.1",
+                HostName = "rabbitmq",
                 Port = 5672,
                 UserName = "guest",
                 Password = "guest"
             };
+            IConnectionFactory connectionFactory2 = new ConnectionFactory
+            {
+                HostName = "rabbitmq-server-0",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            IConnectionFactory connectionFactory3 = new ConnectionFactory
+            {
+                HostName = "rabbitmq.default.svc.cluster.local",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            IConnectionFactory connectionFactory4 = new ConnectionFactory
+            {
+                HostName = "10.152.183.226",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            IConnectionFactory connectionFactory5 = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            List<IConnectionFactory> list = new List<IConnectionFactory>();
+            list.Add(connectionFactory);
+            list.Add(connectionFactory2);
+            list.Add(connectionFactory3);
+            list.Add(connectionFactory4);
+            list.Add(connectionFactory5);
+            foreach (var item in list)
+            {
+                try
+                {
+                    Connection = item.CreateConnection();
+                    Console.WriteLine(Connection.ToString());
+                    break;
+                }
+                catch (Exception)
+                {
 
-            Connection = connectionFactory.CreateConnection();
+                }
+            }
+            //Connection = connectionFactory.CreateConnection();
+            //Connection = connectionFactory5.CreateConnection();
         }
 
         public static RabbitMQConnection Instance
